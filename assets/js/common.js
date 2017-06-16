353ee8aa1de0783c390f4cbcfabcfdd9bab75e82
Siang <siang@alumni.ubc.ca>
2017-06-16 13:45:36 -0700

    fixed mobile

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,3 +6,12 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
 });
+
+function collapseNavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
