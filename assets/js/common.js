f992cc80a812069197ac6e1939711c1b40d25175
Siang <siang@alumni.ubc.ca>
2017-06-28 15:04:58 -0700

    fixed doi

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,9 +5,6 @@ $(document).ready(function() {
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
-    $('a.doi').click(function() {
-        $(this).parent().parent().find(".doi.hidden").toggleClass('open');
-    });    
 });
 
 function collapseNavbar() {
